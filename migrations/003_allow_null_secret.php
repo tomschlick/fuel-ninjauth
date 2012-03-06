db7519d9d1e13be5f9cb50c6430a8aa1cd1a6f8b
Katsuma Ito <katsumai@gmail.com>
2012-03-07 02:41:13 +0900

    'user2' should be 'authentications'.

--- migrations/003_allow_null_secret.php
+++ migrations/003_allow_null_secret.php
@@ -6,14 +6,14 @@ class Allow_null_secret {
 
 	public function up()
 	{
-		\DBUtil::modify_fields('users2', array(
+		\DBUtil::modify_fields('authentications', array(
 			'secret' => array('constraint' => 255, 'type' => 'varchar', 'null' => true),
 		));
 	}
 
 	public function down()
 	{
-		\DBUtil::modify_fields('users2', array(
+		\DBUtil::modify_fields('authentications', array(
 			'secret' => array('constraint' => 255, 'type' => 'varchar', 'null' => false),
 		));
 	}
